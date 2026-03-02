7673e7a62ea7fb57f2b48c28523d60a60e4701e3
pjha26 <praj80624@gmail.com>
2026-03-03 02:30:08 +0530

    feat: Initialize Next.js application with root layout, fonts, providers, and Clerk authentication middleware.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,11 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-import { NextResponse } from 'next/server'
-
 const isPublicRoute = createRouteMatcher(['/', '/sign-in(.*)', '/sign-up(.*)'])
 
 export default clerkMiddleware(async (auth, request) => {
     if (!isPublicRoute(request)) {
         await auth.protect()
     }
-
-    // Allow Clerk Scripts
-    const response = NextResponse.next()
-    response.headers.set(
-        'Content-Security-Policy',
-        "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://clerk.com https://*.clerk.com https://*.clerk.accounts.dev;"
-    )
-    return response
 })
 
 export const config = {
